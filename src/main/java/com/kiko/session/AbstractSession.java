8d40f5f67d05087681fdbe19c5e46a8069b0edc4
wei feng <cqufengwei@icloud.com>
2016-09-22 23:28:05 +0800

    2016/09/22

---
+++ src/main/java/com/kiko/session/AbstractSession.java
@@ -0,0 +1,10 @@
+package com.kiko.session;
+
+import io.netty.channel.Channel;
+
+/**
+ * Created by fengwei on 2016/9/22.
+ */
+public abstract class AbstractSession {
+    public Channel channel;
+}
